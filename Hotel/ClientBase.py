39be4b6ca764485d99805b4a94bf29edc2f3d217
vladakorne <144003918+vladakorne@users.noreply.github.com>
2025-09-16 10:55:48 +0300

    1.4-1.5 Сделаны методы класса валидации

--- Hotel/ClientBase.py
+++ Hotel/ClientBase.py
@@ -1,22 +1,66 @@
 class Client:
-  def __init__(self, surname: str, name: str, phone: str,
-                 patronymic: str = "", passport: str = None, 
-                 email: str = None, comment: str = ""):
-        self._surname = surname
-        self._name = name
-        self._patronymic = patronymic
-        self._passport = passport
-        self._phone = phone 
-        self._email = email
-        self._comment = comment
+    def __init__(self, surname: str, name: str, phone: str,
+                   patronymic: str = "", passport: str = None, 
+                   email: str = None, comment: str = ""):
+          self._surname = self.validate_fio(surname, "Фамилия")
+          self._name = self.validate_fio(name, "Имя")
+          self._patronymic = self.validate_fio(patronymic, "Отчество") if patronymic else ""
+          self._passport = self.validate_passport(passport)
+          self._phone = self.validate_phone(phone)
+          self._email = self.validate_email(email) if email else None
+          self._comment = comment or ""
 
+    @staticmethod
+    def validate_required(value, field_name: str):
+        if not value or not str(value).strip():
+            raise ValueError(f"Поле '{field_name}' не может быть пустым!")
+        return value
+
+    @staticmethod
+    def validate_fio(value: str, field_name: str):
+        value = Client.validate_required(value, field_name)
+        pattern = r"^[А-Яа-яЁёA-Za-z]+$"
+        if not re.match(pattern, value):
+            raise ValueError(f"{field_name} должно содержать только буквы!")
+        return value
+
+    @staticmethod
+    def validate_passport(passport: str):
+        if not passport:
+            return None
+        passport_clean = str(passport).replace(" ", "")
+        pattern = r"^\d{10}$"
+        if not re.match(pattern, passport_clean):
+            raise ValueError("Паспорт должен состоять ровно из 10 цифр!")
+        return passport_clean
+
+    @staticmethod
+    def validate_phone(phone: str):
+        phone = Client.validate_required(phone, "Телефон")
+        phone = str(phone)
+        pattern = r"^\+?\d{7,11}$"
+        if not re.match(pattern, phone):
+            raise ValueError("Телефон должен содержать только цифры и '+' (от 7 до 11 символов)!")
+        return phone
+
+    @staticmethod
+    def validate_email(email: str):
+        if not email:
+            return None
+        pattern = r"^(?!\.)[A-Za-z0-9](?:[A-Za-z0-9._%+-]*)@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"
+        if ".." in email:
+            raise ValueError("Email не может содержать подряд идущие точки!")
+        if not re.match(pattern, email):
+            raise ValueError("Некорректный формат email!")
+        return email
+      
     @property
     def surname(self): 
         return self._surname
 
     @surname.setter
     def surname(self, value: str):
-        self._surname = value
+        self._surname = self.validate_fio(value, "Фамилия")
 
     @property
     def name(self): 
@@ -24,7 +68,7 @@ class Client:
 
     @name.setter
     def name(self, value: str):
-        self._name = value
+        self._name = self.validate_fio(value, "Имя")
 
     @property
     def patronymic(self): 
@@ -32,7 +76,7 @@ class Client:
 
     @patronymic.setter
     def patronymic(self, value: str):
-        self._patronymic = value
+        self._patronymic = self.validate_fio(value, "Отчество) if value else ""
 
     @property
     def passport(self): 
@@ -40,7 +84,7 @@ class Client:
 
     @passport.setter
     def passport(self, value: str):
-        self._passport = value
+        self._passport = self.validate_passport(value)
 
     @property
     def phone(self): 
@@ -48,7 +92,7 @@ class Client:
 
     @phone.setter
     def phone(self, value: str):
-        self._phone = value
+        self._phone = self.validate_phone(value)
 
     @property
     def email(self): 
@@ -56,7 +100,7 @@ class Client:
 
     @email.setter
     def email(self, value: str):
-        self._email = value
+        self._email = self.validate_email(value) if value else None
 
     @property
     def comment(self): 
