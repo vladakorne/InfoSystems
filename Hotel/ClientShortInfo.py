5966494a913b4638eab88944cce4fe67fa82b17b
vladakorne <144003918+vladakorne@users.noreply.github.com>
2025-09-27 16:24:49 +0300

    Исправление: изменена логика наследования

--- Hotel/ClientShortInfo.py
+++ Hotel/ClientShortInfo.py
@@ -1,24 +1,102 @@
-from ClientBase import Client
+import re
+import json
 
-class ClientShort(Client):
+class ClientShort:
     def __init__(self, *args, **kwargs):
-        if kwargs.get("from_client"):
-            client = args[0]
-            if not isinstance(client, Client):
-                raise TypeError("ClientShort.from_client требует объект Client")
-            super().__init__(client.surname, client.name, client.patronymic, client.phone)
+        if kwargs.get("from_string"):
+            if not args:
+                raise ValueError("Для from_string необходимо передать строку первым аргументом")
+            parts = [p.strip() for p in args[0].split(",")]
+            while len(parts) < 4:
+                parts.append("")
+            self._init_short_fields(*parts[:4])
 
-        elif kwargs.get("from_string"):
-            temp_client = Client(args[0], from_string=True)
-            super().__init__(temp_client.surname, temp_client.name, temp_client.patronymic, temp_client.phone)
         elif kwargs.get("from_dict"):
-            temp_client = Client(args[0], from_dict=True)
-            super().__init__(temp_client.surname, temp_client.name, temp_client.patronymic, temp_client.phone)
+            if not args or not isinstance(args[0], dict):
+                raise ValueError("Для from_dict необходимо передать dict первым аргументом")
+            data = args[0]
+            self._init_short_fields(
+                data.get("surname"),
+                data.get("name"),
+                data.get("patronymic", ""),
+                data.get("phone")
+            )
+
         elif kwargs.get("from_json"):
-            temp_client = Client(args[0], from_json=True)
-            super().__init__(temp_client.surname, temp_client.name, temp_client.patronymic, temp_client.phone)
+            if not args:
+                raise ValueError("Для from_json необходимо передать JSON-строку первым аргументом")
+            try:
+                data = json.loads(args[0])
+            except json.JSONDecodeError as e:
+                raise ValueError(f"Некорректный JSON: {e}")
+            if not isinstance(data, dict):
+                raise ValueError("JSON должен содержать объект (dict)")
+
+            self.__init__(data, from_dict=True)
+
         else:
-            raise ValueError("ClientShort можно создать только на основе Client или через from_string/from_dict/from_json")
+            self._init_short_fields(*args)
+
+    def _init_short_fields(self, surname: str, name: str, patronymic: str = "", phone: str = None):
+        self._surname = self.validate_fio(surname, "Фамилия")
+        self._name = self.validate_fio(name, "Имя")
+        self._phone = self.validate_phone(phone)
+        self._patronymic = self.validate_fio(patronymic, "Отчество") if patronymic else ""
+
+    @staticmethod
+    def validate_required(value, field_name: str):
+        if not value or not str(value).strip():
+            raise ValueError(f"Поле '{field_name}' не может быть пустым!")
+        return value
+
+    @staticmethod
+    def validate_fio(value: str, field_name: str):
+        value = ClientShort.validate_required(value, field_name)
+        pattern = r"^[А-Яа-яЁёA-Za-z]+$"
+        if not re.match(pattern, value):
+            raise ValueError(f"{field_name} должно содержать только буквы!")
+        return value
+
+    @staticmethod
+    def validate_phone(phone: str):
+        phone = ClientShort.validate_required(phone, "Телефон")
+        phone = str(phone)
+        pattern = r"^\+?\d{7,11}$"
+        if not re.match(pattern, phone):
+            raise ValueError("Телефон должен содержать только цифры и '+' (от 7 до 11 символов)!")
+        return phone
+
+    @property
+    def surname(self):
+        return self._surname
+
+    @surname.setter
+    def surname(self, value: str):
+        self._surname = self.validate_fio(value, "Фамилия")
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value: str):
+        self._name = self.validate_fio(value, "Имя")
+
+    @property
+    def patronymic(self):
+        return self._patronymic
+
+    @patronymic.setter
+    def patronymic(self, value: str):
+        self._patronymic = self.validate_fio(value, "Отчество") if value else ""
+
+    @property
+    def phone(self):
+        return self._phone
+
+    @phone.setter
+    def phone(self, value: str):
+        self._phone = self.validate_phone(value)
 
     def __str__(self):
         initials = f"{self.name[0]}." if self.name else ""
@@ -27,3 +105,4 @@ class ClientShort(Client):
 
     def __repr__(self):
         return f"ClientShort(surname='{self.surname}', name='{self.name}', patronymic='{self.patronymic}', phone='{self.phone}')"
+
